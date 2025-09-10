e19f9999fce416d2bd600a364df69bc4c9f1870e
Caique-Souza1 <caiquesouzalg@gmail.com>
2025-09-10 13:59:07 -0300

    ajuste no web.php

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
